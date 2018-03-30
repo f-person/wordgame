5f7ae93176afc2ea8036e8860c42cde28c6ca7f0
foreignperson <agakaryan.arshak@gmail.com>
2018-03-31 00:15:30 +0400

    it's working.

--- ps3.py
+++ ps3.py
@@ -4,7 +4,8 @@
 # Created by: Kevin Luu <luuk> and Jenna Wiens <jwiens>
 #
 # Name          : Arshak
-# Time spent    : 1:40 + 2:00 + 0:30 + 2:00 + 2:00
+# Collaborators : Michael
+# Time spent    : 10.5 hours
 
 import math
 import random
@@ -14,7 +15,7 @@ VOWELS = 'aeiou'
 CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 HAND_SIZE = 7
 
-SCRABBLE_LETTER_VALUES = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10,}
+SCRABBLE_LETTER_VALUES = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10, '*':0}
 
 # -----------------------------------
 # Helper code
@@ -90,7 +91,7 @@ def get_word_score(word, n):
     """
     s=0
     for i in word.lower():
-        s+= SCRABBLE_LETTER_VALUES.get(i,0)#if i in SCRABBLE_LETTER_VALUES.keys(): s+=SCRABBLE_LETTER_VALUES[i]
+        s+= SCRABBLE_LETTER_VALUES.get(i,0)
     return s*max(1, 7*len(word)-3*(n-len(word)))
     
 #
@@ -139,7 +140,7 @@ def deal_hand(n):
         x = random.choice(VOWELS)
         hand[x] = hand.get(x, 0) + 1
 
-    hand["*"] = 0
+    hand["*"] = 1
     
     for i in range(num_vowels, n):    
         x = random.choice(CONSONANTS)
@@ -176,13 +177,6 @@ def update_hand(hand, word):
         if hand.get(i,-1)==0:
             del(hand[i])
     return hand
-####    hand = hand.copy()
-####    for i in word.lower():
-####        if i in hand.keys():
-####            hand[i] = hand.get(i, 0) - 1
-####        if hand[i]==0: del hand[i]
-####    return hand
-##print(update_hand({'a':1, 'q':1, 'l':2, 'm':1, 'u':1, 'i':1}, 'quail'))
 #
 # Problem #3: Test word validity
 #
@@ -219,14 +213,11 @@ def is_valid_word(word, hand, word_list):
     word_list: list of lowercase strings
     returns: boolean
     """
-
-##    if word.lower() not in word_list: return False
-##    if len(word.lower())>=len(hand):
-##        return all(i in word.lower() for i in hand)
-##    return False 
     
     word=word.lower()
     number=0
+    x=0
+    t=0
     if word in word_list:#not in word_list: return False
         for i in word:
             if i in hand.keys():
@@ -239,9 +230,15 @@ def is_valid_word(word, hand, word_list):
         if not show_possible_matches(word, word_list):
             return False
         else:
+            for k in word:
+                if k =='*':
+                    x+=1
+                    if x>1: return False
             for i in show_possible_matches(word, word_list):
-                if i[word.index('*')] in VOWELS:
-                     return True
+                if i[word.index('*')] in VOWELS and '*' in hand:
+                    for s in i:
+                        if s!='*' and s in hand: t+=1
+                        if t==len(word)-1: return True
                 else:
                      return False
     return False
@@ -290,7 +287,6 @@ def play_hand(hand, word_list):
       
     """
     totalscore=0
-##    display_hand(hand)
     while calculate_handlen(hand)>0:
         print('Current Hand: ', end='')
         display_hand(hand)
@@ -306,14 +302,10 @@ def play_hand(hand, word_list):
             else:
                 print("That is not a valid word. Please choose another word.")
         print()
-##        print('before', hand)
         hand=update_hand(hand, word)
-##        print('after', hand)
     if len(hand)<=0: print("Ran out of letters. Total score: ", totalscore, 'points.')
     else: print( "Total score: ", totalscore, "points.")
     return totalscore
-##play_hand({'a':1, 'j':1, 'e':1, 'f':1, '*':1, 'r':1, 'x':1}, load_words())
-####play_hand({'a':1, 'c':1, 'f':1, 'i':1, '*':1, 't':1, 'x':1}, load_words())
 
     # BEGIN PSEUDOCODE <-- Remove this comment when you implement this function
     # Keep track of the total score
@@ -390,7 +382,6 @@ def substitute_hand(hand, letter):
             hand.update(d1)
             del hand[letter]
             return hand
-##print(substitute_hand({'a':10, 'd':2, 'r':1, 'g':3}, 'g'))
     
 def play_game(word_list):
     """
@@ -422,14 +413,14 @@ def play_game(word_list):
 
     word_list: list of lowercase strings
     """
-        
-    numhand=int(input("Enter total number of hands: "))
+    numhand=input("Enter total number of hands: ")
     totalscore=0
     n=1
-    for i in range(numhand):
+    for i in range(int(numhand)):
         hand=deal_hand(HAND_SIZE)
-        print('Current Hand: ', end='')
-        display_hand(hand)
+        if totalscore==0:
+            print('Current Hand: ', end='')
+            display_hand(hand)
         if n>0:
             s=''
             while s not in ['yes','no']:
@@ -438,8 +429,13 @@ def play_game(word_list):
                     n-=1
                     letter= input("Which letter would you like to replace: ")
                     hand=substitute_hand(hand,letter)
+            print()
         totalscore+=play_hand(hand, word_list)
         print('----------')
+        replay=''
+        while replay not in ['yes', 'no']:
+            replay=input("Would you like to replay the hand? ")
+        if replay=='yes': totalscore+=play_hand(hand, word_list)
     print("Total score over all hands:", totalscore)
 #
 # Build data structures used for entire session and play game
